595950671ece78dd0e12c82c4ab5d7b094f94e2d
Austin Fraley <fraley.a@c0709a-s16.ufhpc>
2025-02-04 04:24:59 -0500

    updated with the new TNGini.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import numpy as np
-from TNGini import gini_over_time
+from TNGini import gini_evolution
 import sys
 
-# Subbox directories: Alter these as needed.
+# Subbox directories: Alter these as needed
 subbox0 = '/orange/lblecha/IllustrisTNG/Runs/TNG100-1/postprocessing/SubboxSubhaloList/subbox0_99.hdf5' # 7.5 cMpc/h
 subbox1 = '/orange/lblecha/IllustrisTNG/Runs/TNG100-1/postprocessing/SubboxSubhaloList/subbox1_99.hdf5' # 7.5 cMpc/h
 
-# Subbox centers from the TNG website.
+# Subbox centers from the TNG website
 TNG100_subbox0_centerer = np.array([9000, 17000, 63000], dtype=int)
 TNG100_subbox1_centerer = np.array([37000, 43500, 67500], dtype=int)
 
-# Merger tree directories: Alter the base_path as needed.
+# Merger tree directories: Alter base_path as needed
 base_path = '/orange/lblecha/IllustrisTNG/Runs/TNG100-1/output'
 
 if __name__ == "__main__":
 
-    if len(sys.argv) >= 6:
+    if len(sys.argv) >= 7:
         initial_snap = int(sys.argv[1])
         final_snap = int(sys.argv[2])
         subhalo_id = int(sys.argv[3])
         subbox_num = int(sys.argv[4])
         
-        # Interpret the boolean argument for sys.argv.
-        rotate = sys.argv[5].lower()  # Convert to lowercase for case-insensitive matching.
+        # Interpret the boolean argument for sys.argv
+        rotate = sys.argv[5].lower()  # Convert to lowercase for case-insensitive matching
         if rotate in ('true', '1', 'yes', 'y'):
             change_viewing_angle = True
         elif rotate in ('false', '0', 'no', 'n'):
@@ -31,25 +31,24 @@ if __name__ == "__main__":
             print(f'Invalid boolean value for change_viewing_angle: {sys.argv[5]}')
             sys.exit()
             
-        save_path = str(sys.argv[6])
+        save_path = sys.argv[6]
+        save_name = sys.argv[7]
         
-        if len(sys.argv) > 7:
+        if len(sys.argv) > 8:
             print('Too many command line args ({sys.argv}).')
             sys.exit()
 
     else:
-        print('Expecting 6 command line args: initial_snap, final_snap, subhalo_id, subbox_num, rotate, save_path.')
+        print('Expecting 7 command line args: initial_snap, final_snap, subhalo_id, subbox_num, rotate, save_path, save_name.')
         sys.exit()
-    #print(f'sys.argv params: {initial_snap}, {final_snap}, {subhalo_id}, {subbox_num}, {change_viewing_angle}, {save_path}')
     
-    # Define snap range to pass to gini function.
     snap_range = np.arange(initial_snap, final_snap + 1)
+    p_type = '4' # stellar particles
     
-    # Randomly rotate the plot if desired.
+    # Randomly rotate the plot if desired
     if rotate:
-        cos_theta = np.random.uniform(-1, 1)
-        theta = np.arccos(cos_theta)
-        phi = np.random.uniform(0, 2 * np.pi)
-        gini_over_time(base_path, subbox1, subhalo_id, snap_range, '4', save_path, subbox_num=subbox_num, change_viewing_angle=change_viewing_angle, theta=theta, phi=phi)
+        gini_evolution(base_path, subbox1, subhalo_id, snap_range, p_type, subbox_num=subbox_num, change_viewing_angle=change_viewing_angle, save_path=save_path, save_name=save_name)
+        
+    # Default viewing plane
     else:
-        gini_over_time(base_path, subbox1, subhalo_id, snap_range, '4', save_path, subbox_num=subbox_num)
+        gini_evolution(base_path, subbox1, subhalo_id, snap_range, p_type, subbox_num=subbox_num, save_path=save_path, save_name=save_name)
