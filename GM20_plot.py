3885b191fed6f25f8bdf23db1a3a75ee0c3a3454
Austin Fraley <fraley.a@c0709a-s8.ufhpc>
2025-02-05 04:23:20 -0500

    updated with random rotations

--- GM20_plot.py
+++ GM20_plot.py
@@ -1,5 +1,5 @@
 import numpy as np
-from TNGini import G_vs_M20_plot
+from TNGini import giniM20_plot
 import sys
 
 # Merger tree directories: Alter the base_path as needed
@@ -8,20 +8,28 @@ base_path = '/orange/lblecha/IllustrisTNG/Runs/TNG100-1/output'
 if __name__ == "__main__":
 
     if len(sys.argv) >= 7:
-        initial_snap = int(sys.argv[1])
-        final_snap = int(sys.argv[2])
-        subhalo_id = int(sys.argv[3])
-        subbox_num = int(sys.argv[4])
-        subbox_path = sys.argv[5]
-        save_name = sys.argv[6]
+        subbox_path = str(sys.argv[1])
+        initial_snap = int(sys.argv[2])
+        final_snap = int(sys.argv[3])
+        subhalo_id = int(sys.argv[4])
+        subbox_num = int(sys.argv[5])
+        rotate = str(sys.argv[6])
+        
+        if rotate.lower() in ['true', 'yes', 'y', '1']:
+            change_viewing_angle = True
+        elif rotate.lower() in ['false', 'no', 'n', '0']:
+            change_viewing_angle = False
+        
+        save_name = str(sys.argv[7])
         
         if len(sys.argv) > 8:
             print('Too many command line args ({sys.argv}).')
             sys.exit()
 
     else:
-        print('Expecting 7 command line args: initial_snap, final_snap, subhalo_id, subbox_num, subbox_path, save_name.')
+        print('Expecting 7 command line args: subbox_path, initial_snap, final_snap, subhalo_id, subbox_num, rotate, save_name.')
         sys.exit()
     
     snap_range = np.arange(initial_snap, final_snap)
-    G_vs_M20_plot(base_path, subbox_path, subhalo_id, snap_range, subbox_num=subbox_num, save_name=save_name)
+    p_type = '4'
+    giniM20_plot(base_path, subbox_path, subhalo_id, snap_range, p_type, subbox_num=subbox_num, change_viewing_angle=change_viewing_angle, save_name=save_name)
